f2a9b217a7e21383135df60a6f8486f5c07a454a
Facundo Giuliano <facugiuliano10@gmail.com>
2024-11-25 20:58:59 -0300

    anunciar ganador

--- javascript.js
+++ javascript.js
@@ -1,10 +1,3 @@
-
-let i = 0;
-let humanScore = 0;
-let computerScore = 0;
-let playerSelection = "--";
-let computerChoice = "--";
-
 function getComputerChoice() {
     let choice = Math.random();
     if (choice < 0.3)
@@ -16,39 +9,56 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerChoice) {
-    i++;
-
-    juego.textContent = `Seleccion jugador: ${playerSelection}
+    while (computerScore <= 5 && humanScore <= 5) {
+        juego.textContent = `Seleccion jugador: ${playerSelection}
 
 Seleccion computadora: ${computerChoice}
 
 `;
 
-    content.textContent = `Puntaje jugador: ${humanScore}
+        content.textContent = `Puntaje jugador: ${humanScore}
 
 Puntaje computadora: ${computerScore}
 
 `;
 
-    if (playerSelection === "piedra" && computerChoice === "papel") {
-        computerScore++;
-        return presultados.textContent = "Resultado: ¡Pierdes! El papel vence a la piedra";
-    }
-    else if (playerSelection === "papel" && computerChoice === "tijera") {
-        computerScore++;
-        return presultados.textContent = "Resultado: ¡Pierdes! La tijera vence al papel"
-    }
-    else if (playerSelection === "tijera" && computerChoice === "piedra") {
-        computerScore++;
-        return presultados.textContent = "Resultado: ¡Pierdes! La piedra vence a la tijera";
-    }
-    else if (playerSelection === computerChoice) {
-        return presultados.textContent = "Resultado: Empate !";
+        if (playerSelection === "piedra" && computerChoice === "papel") {
+            computerScore++;
+            return presultados.textContent = "Resultado: ¡Pierdes! El papel vence a la piedra";
+        }
+        else if (playerSelection === "papel" && computerChoice === "tijera") {
+            computerScore++;
+            return presultados.textContent = "Resultado: ¡Pierdes! La tijera vence al papel"
+        }
+        else if (playerSelection === "tijera" && computerChoice === "piedra") {
+            computerScore++;
+            return presultados.textContent = "Resultado: ¡Pierdes! La piedra vence a la tijera";
+        }
+        else if (playerSelection === computerChoice) {
+            return presultados.textContent = "Resultado: Empate !";
+        }
+        else
+            humanScore++;
+        return presultados.textContent = "Resultado: Ganaste !";
+    } if (humanScore == 5) {
+        presultados.textContent = `GANADOR: Player`;
     }
     else
-        humanScore++;
-    return presultados.textContent = "Resultado: Ganaste !";
+        presultados.textContent = `GANADOR: Computer`;
 }
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.id, getComputerChoice());
+    });
+});
+
+let humanScore = 0;
+let computerScore = 0;
+let playerSelection = "--";
+let computerChoice = "--";
 
 const resultados = document.querySelector("#resultados");
 
@@ -78,11 +88,3 @@ Seleccion computadora: ${computerChoice}
 resultados.appendChild(presultados);
 resultados.appendChild(juego);
 resultados.appendChild(content);
-
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-        playRound(button.id, getComputerChoice());
-    });
-});
